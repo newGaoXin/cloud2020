39e28ad6e7dfc3c309c6357f5c4505a8a8b70077
gaoxin <492706907@qq.com>
2020-12-28 21:40:37 +0800

    2020-12-26 22:35 完成p80 Bus动态刷新全局广播的设计思想和选型

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -1,4 +1,4 @@
-package com.atguigu.config;
+package com.atguigu.springcloud.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
