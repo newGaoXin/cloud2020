7a14963e2e3e56cb41f5851d85382f8462a619d4
gaoxin <492706907@qq.com>
2020-12-29 21:36:11 +0800

    2020-12-26 22:35 完成p82 Bus动态刷新定点通知

--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain336 {
+public class ConfigClientMain3366 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain336.class,args);
+        SpringApplication.run(ConfigClientMain3366.class,args);
     }
 }
