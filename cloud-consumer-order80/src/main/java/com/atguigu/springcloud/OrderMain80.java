a6c7f3c9957f6842e3c891adeaf5a26089a83ebd
gaoxin <492706907@qq.com>
2021-01-02 22:27:29 +0800

    2021-1-2 22:27 完成p100 Nacos服务中心对比提升

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -9,9 +9,9 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 @SpringBootApplication
 @EnableEurekaClient
 @RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
-public class OrderMain {
+public class OrderMain80 {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain.class,args);
+        SpringApplication.run(OrderMain80.class,args);
     }
 }
